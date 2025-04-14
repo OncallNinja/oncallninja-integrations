ce025f81727158197aeb40f1454dec163548e5f8
aayuguptml <aayuguptml@gmail.com>
2025-04-13 20:08:39 -0700

    Call ActionRouter base class in Kibana

--- oncallninja_integrations/kibana.py
+++ oncallninja_integrations/kibana.py
@@ -23,6 +23,7 @@ class KibanaClient(ActionRouter):
             'Content-Type': 'application/json'
         })
         self.logger = logging.getLogger(__name__)
+        super.__init__()
     
     @action(description="KIBANA API: Make HTTP request.")
     def _make_request(self, method: str, path: str, params: Optional[Dict] = None, 
